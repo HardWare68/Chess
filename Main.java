c5e76d6bb1f3ac956aae64bc25e9a6288bf0a48c
HardWare <47306310+HardWare68@users.noreply.github.com>
2021-09-16 17:43:38 +0000

    added rookMoves and the sort

--- Main.java
+++ Main.java
@@ -4,15 +4,28 @@ class Main {
   public static void main(String[] args) {
     //declaring variables
     boolean boolIsGameWon = false;
+    byte playerTurn = 1;
+    String nextMove;
 
     //importing local files
     boardManager boardManager = new boardManager();
+    Scanner scan = new Scanner(System.in);
 
     System.out.println("Hello world!");
     boardManager.printBoard();
     
-
     while (boolIsGameWon == false){
+      //get the next move from the user
+      if(playerTurn == 1){
+        System.out.println("It's player 1's turn!");
+      } else {
+        System.out.println("It's player 2's turn!");
+      }
+      System.out.println("Enter your move! Format it in PGN format. (PGN formation is like so: (letter of piece)(file you wish to move to)(rank you wish to move to)");
+      nextMove = scan.nextLine();
+
+
+    boardManager.performMove(nextMove);
 
     }
   }
--- boardManager.java
+++ boardManager.java
@@ -19,4 +19,55 @@ public class boardManager{
       System.out.println();
     }
   }
+
+  public void performMove(String paramNextMove){
+    //first, the move is gonna be given to us in probably PGN form, so let's check to make sure it's in that. That'll probably help.
+    isPGN(paramNextMove);
+    
+
+    //next, check to make sure the piece can actually move to that square
+    //who's ready for some suffering?
+    isLegalMove(paramNextMove);
+  }
+
+  private boolean isPGN(String paramNextMove){
+      if(paramNextMove.matches("[NKQRB]?[A-H][1-8]")){
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  private boolean isLegalMove(String paramNextMove){
+    //first, lets find out what piece we're dealing with.
+
+
+    if(paramNextMove.charAt(0) == 'R'){
+      //piece is a rook, lets find out if its a legal move
+      rookMoves(paramNextMove);
+    }
+
+  }
+
+  private boolean rookMoves(String paramNextMove){
+    //first, lets find the rooks on the board
+    //there will likely be two, so we have to be prepared for both of them
+    for(int row=0; row < chessBoard.length; row++){
+      for(int column=0; column < chessBoard[row].length; column++){
+        if(chessBoard[row][column].toUpperCase() == "R"){
+          //we found a rook. lets find out where they want to place it, and if it is legal
+
+          //lets grab the second and third characters since thatll tell us where they want to go
+
+          int file = paramNextMove.charAt(1); //the file is given to us as a letter. let's make that into a number!
+
+          int rank = paramNextMove.charAt(2);
+        }
+      }
+    }
+  }
+
+  private int fileToNumber(char paramFile){
+    if(paramFile.toUpperCase == 'A'){return 0;}
+  }
 }
